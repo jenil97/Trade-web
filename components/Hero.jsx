298b2fec95923b55b3f168003d08eafb16263e68
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-11-05 17:18:42 +0530

    fix : Hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -11,27 +11,25 @@ const Hero = () => {
         loop
       />
       <div className="w-full h-[90%] flex flex-col justify-center items-center text-white px-4 text-center font-Raleway">
-        <h1 className="text-4xl font-normal cursor-default">Decentralized</h1>
-        <h1 className="py-2 text-4xl font-normal cursor-default">
+        <h1 className="text-5xl font-normal cursor-default">Decentralized</h1>
+        <h1 className="py-2 text-5xl font-normal cursor-default">
           <span className="text-[#00d8ff] font-medium">Trading</span> Protocol
         </h1>
         <p className="text-xl py-4 cursor-default">
           Guaranteed Liquidity Trading for Millions of Users and Top Ethereum
           Applications.
         </p>
-        <div>
-          <button className="m-2 bg-gradient-to-r from-[#6a00ff] to-[#00d8ff] py-3 px-7 text-xl rounded-3xl">
+        <p className="text-center text-white text-xl font-medium font-Raleway cursor-default md:-mt-2 mt-3">
+          Total Volume <span className="text-[#00d8ff]">Secured :</span> $42,104,783,662.47
+        </p>
+        <div className="md:mt-2 mt-8">
+          <button className="m-3 bg-gradient-to-r from-[#6a00ff] to-[#00d8ff] py-3 px-7 text-xl rounded-3xl">
             About
           </button>
-          <button className="m-2 bg-gradient-to-r from-[#6a00ff] to-[#00d8ff] py-3 px-7 text-xl rounded-3xl">
+          <button className="m-3 bg-gradient-to-r from-[#6a00ff] to-[#00d8ff] py-3 px-7 text-xl rounded-3xl">
             FAQ
           </button>
         </div>
-      </div>
-      <div>
-        <p className="text-center text-white text-2xl font-semibold font-Raleway cursor-default">
-          Total Volume Secured: $42,104,783,662.47
-        </p>
       </div>
     </div>
   );
